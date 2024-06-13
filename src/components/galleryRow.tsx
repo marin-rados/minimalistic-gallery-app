72d977f1518580cda8ec34e1676673bb113d6eaf
Marin Rados <radosmarin35@gmail.com>
2024-06-13 17:55:06 +0200

    More responsive adjustmens add image zoom on click

--- src/components/galleryRow.tsx
+++ src/components/galleryRow.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ImagesType } from "../types/global";
 
 type Props = {
@@ -5,19 +6,43 @@ type Props = {
 };
 
 const GalleryRow = ({ data }: Props) => {
+  const [activeItem, setActiveItem] = useState<ImagesType | null>(null);
+
+  const handleActiveItem = (image: ImagesType) => {
+    setActiveItem(image);
+  };
+
   return (
-    <div className="gallery-row">
-      {data.map((image) => {
-        return (
-          <img
-            key={image.image}
-            src={image.image}
-            alt="Image"
-            className="gallery-row__image"
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="gallery-row">
+        {data.map((image) => {
+          return (
+            <img
+              onClick={() => handleActiveItem(image)}
+              key={image.image}
+              src={image.image}
+              alt="Image"
+              className="gallery-row__image"
+            />
+          );
+        })}
+      </div>
+      {activeItem && (
+        <>
+          <div
+            className="modal"
+            style={{ backgroundImage: `url(${activeItem.image})` }}
+          >
+            <div className="modal__close" onClick={() => setActiveItem(null)}>
+              X
+            </div>
+
+            {/* <img src={activeItem.image} alt="Image" /> */}
+          </div>
+          <div className="overlay" onClick={() => setActiveItem(null)}></div>
+        </>
+      )}
+    </>
   );
 };
 
